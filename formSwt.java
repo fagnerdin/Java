4b08f81ce0afd54f99dd6f15ad2166375dc8ee43
fagnerdin <fag.dasilva@gmail.com>
2015-10-29 21:59:56 -0200

    Update formSwt.java
    Adicionado comentarios

--- formSwt.java
+++ formSwt.java
@@ -22,26 +22,32 @@ public class MyFirstSwtForm {
         Display display = new Display();
         final Shell shell = new Shell(display);
         
+        // INSTANCIA LABEL
         Label label = new Label(shell, SWT.WRAP);
-        label.setText("Javaaaaaaaaaaaaaaaaaaaaaaaa!");
+        label.setText("Javaaaaaaaaaaaaaaaaaaaaaaaa!"); // TEXT OF LABEL
         
+        // BOTOES
         Button button1 = new Button(shell, SWT.PUSH);
         button1.setText("Botao 1");
         Button button2 = new Button(shell, SWT.PUSH);
         button2.setText("Botao 2");
 
-        final int insetX = 4, insetY = 4;
+        
+        // MONTA LAYOUT
+        final int insetX = 4, insetY = 4; // ESPACO INTERNO
         FormLayout formLayout = new FormLayout();
         formLayout.marginWidth = insetX;
         formLayout.marginHeight = insetY;
         shell.setLayout(formLayout);
 
+        // POSICAO E TAMANHO DO FORM
         Point size = label.computeSize(SWT.DEFAULT, SWT.DEFAULT);
         final FormData labelData = new FormData(size.x, SWT.DEFAULT);
         labelData.left = new FormAttachment(0, 0);
         labelData.right = new FormAttachment(100, 0);
         label.setLayoutData(labelData);
         
+        // MONTA ESPACO INTERNO
         shell.addListener(SWT.Resize, new Listener() {
           public void handleEvent(Event e) {
             Rectangle rect = shell.getClientArea();
@@ -50,16 +56,21 @@ public class MyFirstSwtForm {
           }
         });
 
+        
+        // MONTA BOTAO 2
         FormData button2Data = new FormData();
         button2Data.top = new FormAttachment(label, insetY);
         button2Data.right = new FormAttachment(100, -insetX);
         button2Data.bottom = new FormAttachment(100, 0);
         button2.setLayoutData(button2Data);
 
+        // MONTA BOTAO 1
         FormData button1Data = new FormData();
+        button2Data.top = new FormAttachment(label, insetY);
         button1Data.right = new FormAttachment(button2, -insetX);
         button1Data.bottom = new FormAttachment(100, 0);
         button1.setLayoutData(button1Data);
+        
 
         shell.pack();
         shell.open();
@@ -69,4 +80,4 @@ public class MyFirstSwtForm {
         }
         display.dispose();
     }
-}
+}                       
